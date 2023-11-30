735dc781e71d9eef43108b33d66872d4e2367877
taha-tecxar <taha.virdiwala@tecxar.io>
2023-11-30 10:31:55 +0530

    changes

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as auth from "./src/auth.interface";
 import * as agentStatus from "./src/agentStatus.interface"
+import * as customer from "./src/customers.interface";
 
-export default { ...auth , ...agentStatus }
-
+export default { ...auth , ...agentStatus, ...customer }
