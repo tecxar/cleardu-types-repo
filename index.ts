0d60dbf26816221ea9b46944fb62f3e09aa7e40f
taha-tecxar <taha.virdiwala@tecxar.io>
2023-12-28 10:21:40 +0530

    change

---
+++ index.ts
@@ -0,0 +1,4 @@
+import * as auth from "./src/auth.interface";
+import * as main from "./src/main.type";
+
+export default { ...auth, ...main };
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import * as auth from "./auth.interface";
-import * as main from "./main.type";
-
-export default { ...auth, ...main };
