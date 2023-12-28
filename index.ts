585be2e618666f9f5c2040a5bae9371356476728
taha-tecxar <taha.virdiwala@tecxar.io>
2023-12-28 09:52:02 +0530

    changes

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
-// import * as auth from "./src/auth.interface";
-// import * as main from "./src/main.type";
+import * as auth from "./src/auth.interface";
+import * as main from "./src/main.type";
 
-export type  * from "./src/auth.interface"
-export type  * from "./src/main.type"
-
-
-// export * from "./src/auth.interface"
-// export * from "./src/main.type"
+export default  {...auth, ...main} 
 
--- src/main.type.ts
+++ src/main.type.ts
@@ -1,11 +1,13 @@
-export interface ResponseCountRows<T> {
+interface ResponseCountRows<T> {
   count: number;
   rows: T;
 }
 
 
-export interface Response<T> {
+interface Response<T> {
   status: boolean;
   data: ResponseCountRows<T> | null;
   message: string;
 }
+
+export { ResponseCountRows, Response }
