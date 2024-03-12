295b525463ca27d73b2ad04cc45e5ee538977481
taha-tecxar <taha.virdiwala@tecxar.io>
2024-03-12 11:27:36 +0530

    add processlist types

--- src/process.interface.ts
+++ src/process.interface.ts
@@ -13,6 +13,81 @@
     length?: number;
   }
 
+
+  interface WorkflowSubcategory {
+    name: string;
+    dpd: number | undefined;
+    templateId: number | undefined;
+    requireSign: string | boolean;
+    isActive: string | boolean;
+    workflowSubcategoryId?: number;
+    workflowCategoryId?: number;
+    label: string;
+    userSignRole?: string;
+  }
+
+  interface ProcessList {
+    id?: number;
+    name: string | undefined;
+    processName: string;
+    dialerProcessId?: number;
+    dialerProcess?: string;
+    clientId?: number;
+    dialerId?: number;
+    isActive?: boolean | number;
+    processType: string;
+    isCronActive?: boolean | number;
+    maxRatio?: number;
+    agents?: any;
+    dial_method?: any;
+  }
+
+  interface ProcessConfigList {
+    id?: number;
+    clientId?: number;
+    processName?: string | any;
+    posMin?: number;
+    posMinUnit?: string;
+    posMax?: number;
+    posMaxUnit?: string;
+    loanAmountMin?: number;
+    loanAmountMinUnit?: string;
+    loanAmountMax?: number;
+    loanAmountMaxUnit?: string;
+    isCompleted?: boolean | undefined | number;
+    maxRatio?: number;
+    dialerId?: number;
+    dialerProcess?: string;
+    threshold?: number;
+    durationStartDate?: Date;
+    durationEndDate?: Date;
+    isProcessRollover?: boolean;
+    isSettlementRollover?: boolean;
+    allowSettlement?: boolean;
+    variance?: number;
+    isCollectionsPlatform?: boolean;
+    isClientPortal?: boolean;
+    isLegalPlatform?: boolean;
+    isNumberMasking?: boolean;
+    isCustomerSearchable?: boolean;
+    targetDaily?: number;
+    targetWeekly?: number;
+    targetMonthly?: number;
+    productId?: number;
+    dialerProcessId?: number;
+    prouctTypeId?: number;
+    workflowCategoryId?: number;
+    workflowSubcategoryData?: WorkflowSubcategory[] | undefined;
+    workflowName?: string;
+    agents?: string[];
+    platformType?: string;
+    isActive?: boolean | number;
+    tab?: string;
+    authOfficerId?: number | null;
+    arbitratorId?: number | null;
+    isLoading?: boolean;
+  }
+
    interface ProcessData extends Process {
     processConfigId: number;
   }
@@ -58,4 +133,4 @@
     workflowCategoryId?: number;
   }
 
-  export { Process, ProcessData, ConfigPayload, ProcessConfig }
+  export { Process, ProcessData, ConfigPayload, ProcessConfig, ProcessList, WorkflowSubcategory, ProcessConfigList }
